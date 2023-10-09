cf46ae52a39e25308d3d56b28a94c8820242e726
Seppe Van Campfort <r0929446@student.thomasmore.be>
2023-10-09 16:11:01 +0200

    aanpassingen, maar werkt niet

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,10 @@
-<h1>The Vinyl Shop</h1>
+<x-vinylshop-layout>
+    <h1>The Vinyl Shop</h1>
 
-<p>Welcome to the website of The Vinyl Shop, a large online store with lots of (classic) vinyl records.</p>
-//link with URL
-<a href="/contact">Contact</a>
+    <p>Welcome to the website of The Vinyl Shop, a large online store with lots of (classic) vinyl records.</p>
+    //link with URL
+    <a href="/contact">Contact</a>
 
-//Link with route name
-<a href="{{ route('contact') }}">Contact</a>
+    //Link with route name
+    <a href="{{ route('contact') }}">Contact</a>
+</x-vinylshop-layout>
